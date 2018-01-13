bf2bdfbb9ba82f3a94dd41be98c129d0dd11638c
bethschoenfeld <bethingraham3@gmail.com>
2018-01-13 10:28:47 -0500

    fixing git

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Movie from './components/Movie';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Movie />
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Review.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Review extends Component {
+    render() {
+        return (
+
+            <div>
+                Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua.
+            Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo
+            consequat.
+            </div>
+
+        )
+    }
+}
+export default Review
